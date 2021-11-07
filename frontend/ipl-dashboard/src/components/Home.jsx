20b549835601942ebd31aaf36aea0db35827157f
Praveen Kumar <pruvi007@gmail.com>
2021-11-07 11:53:25 +0530

    minor change to test CI

--- frontend/ipl-dashboard/src/components/Home.jsx
+++ frontend/ipl-dashboard/src/components/Home.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 class Home extends Component{
     state = {};
     render(){
-        return <div style={{color:'red'}}>Hello ipl-dashboard</div>
+        return <div style={{color:'red'}}>
+            <h1>Hello ipl-dashboard</h1>
+            Continuos Integration Setup : <span style={{color:'green', fontWeight: 'bold'}}>SUCCESS</span>
+            
+        </div>
     }
 
 }
