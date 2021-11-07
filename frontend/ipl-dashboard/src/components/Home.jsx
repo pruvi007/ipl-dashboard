9417f5a5bc1c74a78908cf59dff6408623af5b75
Praveen Kumar <pruvi007@gmail.com>
2021-11-07 10:59:48 +0530

    minor change to check CI

--- frontend/ipl-dashboard/src/components/Home.jsx
+++ frontend/ipl-dashboard/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 class Home extends Component{
     state = {};
     render(){
-        return <div>Hello ipl-dashboard</div>
+        return <div style={{color:'red'}}>Hello ipl-dashboard</div>
     }
 
 }
